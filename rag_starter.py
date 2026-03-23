72f38ada886c1220fd31967b60f3424d70b3c473
CyberOmerkhan <amazingOmerkhan@gmail.com>
2026-03-23 19:24:59 +0000

    rag index update

--- rag_starter.py
+++ rag_starter.py
@@ -7,27 +7,26 @@ import os
 
 load_dotenv() 
 OPENAI_API_KEY = os.getenv('OPENAI_API_KEY')
-documents = SimpleDirectoryReader("data_confluence").load_data()
+documents = SimpleDirectoryReader("data", recursive=True).load_data()
 index = VectorStoreIndex.from_documents(documents)
 query_engine = index.as_query_engine()
 
 def multiply(a: float, b: float) -> float:
     return a * b
 
-async def search_documents(query: str) -> str:
-    response = await query_engine.aquery(query)
+def search_documents(query: str) -> str:
+    response = query_engine.query(query)  # Use .query() not .aquery()
     return str(response)
 
 agent = FunctionAgent(
     tools = [multiply, search_documents],
-    llm = OpenAI(model='gpt-5.1'),
-    system_prompt="You are a helpful assistant that can perform calculations and search through documents to answer questions"
+    llm = OpenAI(model='gpt-5.2'), 
+    system_prompt="You are a helpful assistant."
 )
 
 async def main():
-    response = await agent.run(
-        "How do I use a printer in Cornell?"
-    )
+    prompt = input()
+    response = await agent.run(prompt)        
     print(response)
 
 if __name__ == '__main__':
